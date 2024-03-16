8f408415faea467ef962937b3900a853aff7a765
RamKishore <mramki1803@gmail.com>
2024-03-16 22:18:23 +0530

    Google Signin button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <div>
+        <LandingPage />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,27 @@
+import googlebtn from "../assets/With Text.svg";
+import axios from "axios";
+
+const LandingPage = () => {
+  const auth = async () => {
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:5000/api/v1/user/request"
+      );
+      const { url } = response.data;
+      window.location.href = url; // Redirect to the Google authentication page
+    } catch (error) {
+      console.log("Error initiating Google Sign-In:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>LandingPage</h1>
+      <button onClick={() => auth()}>
+        <img src={googlebtn} alt="google signin button" />
+      </button>
+    </div>
+  );
+};
+
+export default LandingPage;
