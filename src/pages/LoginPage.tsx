794bc75a2fc41cf3fab38ed9ec1e138c20ae9f59
RamKishore <mramki1803@gmail.com>
2024-04-17 21:35:21 +0530

    .

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,24 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import googlebtn from "../assets/With Text.svg";
 import axios from "axios";
 
 const backendURL = import.meta.env.VITE_BACKEND_URL;
 
 const LoginPage = () => {
+  const [error, setError] = useState("");
+
   useEffect(() => {
-    const fetchData = async () => {
-      const code = encodeURI(
-        new URLSearchParams(window.location.search).get("code") as string
-      );
-      console.log(code);
-      if (code) {
-        const response = await axios.post(
-          `${backendURL}/api/v1/user/oauth?code=${code}`
-        );
-        console.log(response);
-      }
-    };
-    fetchData();
+    const error = new URLSearchParams(window.location.search).get("error");
+    if(error){
+      console.log("Unauthorized email domain")
+      setError("Unauthorized email domain");
+    }
   }, []);
 
   const handleSignIn = async () => {
@@ -36,8 +30,8 @@ const LoginPage = () => {
   return (
     <div className="min-h-screen bg-black text-white flex items-center justify-center">
       <div className="text-center">
-        <h1 className="text-4xl mb-6">Welcome to Your App</h1>
-        <p className="mb-8">Please sign in with your Google account</p>
+        <h1 className="text-4xl mb-6">{error == "" ? "Welcome to Campus Connect" : "Unauthorized Email"}</h1>
+        <p className="mb-8">{error == "" ? "Please sign in with your College Email ID" : "Please use your college Email Id"}</p>
         <button
           onClick={() => handleSignIn()}
           className="flex items-center justify-center bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
