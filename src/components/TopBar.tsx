3193ba24dd1e36b21b9f823d5e3ba3413edfa7cf
RamKishore <mramki1803@gmail.com>
2024-05-08 21:10:35 +0530

    .

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,9 +1,13 @@
+import SearchIcon from './assets/SearchIcon.svg';
+
 const TopBar = () => {
-  return (
-    <nav className="h-16 bg-gray-800 text-white flex items-center justify-between px-4">
-      <div className="flex-grow">TopBar</div>
-    </nav>
-  );
+    return (
+        <nav className="h-16 bg-gray-800 text-white flex items-center justify-between px-4">
+            <div className="flex-grow">
+                <SearchIcon/>
+                TopBar</div>
+        </nav>
+    );
 };
 
 export default TopBar;
