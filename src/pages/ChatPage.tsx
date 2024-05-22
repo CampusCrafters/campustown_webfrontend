948eac1df2c274c8067cd93b732e9f73899695a0
RamKishore <ramkishore@RamKishores-Laptop.local>
2024-05-22 15:53:31 +0530

    chat page

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,7 +1,22 @@
-const ChatPage = () => {
-  return (
-    <div>ChatPage</div>
-  )
-}
+import { ScrollArea } from "@/components/ui/scroll-area";
 
-export default ChatPage
+const ChatPage = () => {
+  const dummyList = Array.from({ length: 100 }, (_, i) => `Item ${i + 1}`);
+
+  return (
+    <div className="flex gap-5">
+      <ScrollArea className="h-[100vh] w-[20%] rounded-md border p-4">
+        {dummyList.map((item, index) => (
+          <div key={index}>{item}</div>
+        ))}
+      </ScrollArea>
+      <ScrollArea className="h-[100vh] w-[80%] rounded-md border p-4">
+        {dummyList.map((item, index) => (
+          <div key={index}>{item}</div>
+        ))}
+      </ScrollArea>
+    </div>
+  );
+};
+
+export default ChatPage;
