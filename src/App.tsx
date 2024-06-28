b778f95840bdbcfd5572fafacaadd253f7f643a4
RamKishore <mramki1803@gmail.com>
2024-06-28 11:27:22 +0530

    added component-names

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import Layout from "./components/Layout";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/Login";
 import ProfilePage from "./pages/Profile";
-import Projects from "./pages/Projects";
+import Projects from "./pages/home/explore-all/Projects";
 import Resumedemo from "./pages/Resumedemo";
 import EventsPage from "./pages/EventsPage";
 import { Provider } from "react-redux";
@@ -46,17 +46,17 @@ function App() {
         <Routes>
           <Route
             path="/"
-            element={!user ? <LandingPage /> : <Navigate to="/projects" />}
+            element={!user ? <LandingPage /> : <Navigate to="/explore-all" />}
           />
           <Route
             path="/login"
-            element={!user ? <LoginPage /> : <Navigate to="/projects" />}
+            element={!user ? <LoginPage /> : <Navigate to="/explore-all" />}
           />
           <Route
             path="/"
             element={user ? <Layout /> : <Navigate to="/login" />}
           >
-            <Route path="projects" element={<Projects />} />
+            <Route path="explore-all" element={<Projects />} />
             <Route path="profile" element={<ProfilePage />} />
             <Route path="chat" element={<ChatPage />} />
             <Route path="resume" element={<Resumedemo />} />
