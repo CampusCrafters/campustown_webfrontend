b7c673a46ecc17b16ee044a0400ce9cefde3c6cf
RamKishore <mramki1803@gmail.com>
2024-07-02 13:45:16 +0530

    members page

--- src/components/MemberCard.tsx
+++ src/components/MemberCard.tsx
@@ -1,6 +1,5 @@
 import ProfileIcon from "./custom-ui/profile-icon";
 import YearPill from "./custom-ui/year-pill";
-import dropdownArrow from "../assets/icons/heroicons-mini/chevron-down.svg";
 
 const MemberCard: React.FC<MemberCardProps> = ({ src, name, batch, role }) => {
   return (
@@ -17,7 +16,6 @@ const MemberCard: React.FC<MemberCardProps> = ({ src, name, batch, role }) => {
         <div style={nameStyles}>{name}</div>
         <YearPill batch={batch} />
       </div>
-      <img src={dropdownArrow} alt="dropdown arrow" />
       <div
         style={{
           display: "flex",
--- src/pages/home/my-listings/manage-project/Members.tsx
+++ src/pages/home/my-listings/manage-project/Members.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState, AppDispatch } from "@/redux/store";
 import { getProjectWithId } from "@/redux/projects/projectsActions";
-import { fetchUserProfile } from "@/redux/users/profileActions";
+import MemberCard from "../../../../components/MemberCard"; 
 
 function Members() {
   const [searchParams] = useSearchParams();
@@ -13,7 +13,6 @@ function Members() {
   const projectDetails = useSelector(
     (state: RootState) => state.projects.projectDetails
   );
-  const [userProfiles, setUserProfiles] = useState<{ [key: number]: any }>({});
 
   useEffect(() => {
     if (projectID) {
@@ -21,70 +20,23 @@ function Members() {
     }
   }, [dispatch, projectID]);
 
-  useEffect(() => {
-    const fetchProfiles = async () => {
-      if (projectDetails?.members) {
-        const profiles: { [key: number]: any } = {};
-        for (const member of projectDetails.members) {
-          try {
-            const response = (await dispatch(
-              fetchUserProfile(member.user_id)
-            )) as any;
-            profiles[member.user_id] = response.name;
-          } catch (error) {
-            console.error("Error fetching user profile:", error);
-          }
-        }
-        setUserProfiles(profiles);
-      }
-    };
-
-    if (projectDetails) {
-      fetchProfiles();
-    }
-  }, [dispatch, projectDetails]);
-
   return (
     <div>
       {projectDetails ? (
-        <div
-          style={{
-            background: "white",
-            padding: "20px",
-            borderRadius: "5px",
-            boxShadow: "0 0 5px rgba(0, 0, 0, 0.1)",
-          }}
-        >
+        <div>
           {projectDetails.members && projectDetails.members.length > 0 ? (
             <div>
-              <span className="font-bold">Project Members:</span>
-              <table
-                style={{ width: "100%", borderCollapse: "collapse" }}
-                className="mb-3"
-              >
-                <thead>
-                  <tr>
-                    <th style={{ border: "1px solid black", padding: "8px" }}>
-                      Username
-                    </th>
-                    <th style={{ border: "1px solid black", padding: "8px" }}>
-                      Role
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {projectDetails.members.map((member: any, index: any) => (
-                    <tr key={index}>
-                      <td style={{ border: "1px solid black", padding: "8px" }}>
-                        {userProfiles[member.user_id] || "Loading..."}
-                      </td>
-                      <td style={{ border: "1px solid black", padding: "8px" }}>
-                        {member.role}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+              <div className="mb-3 flex flex-col gap-[12px]">
+                {projectDetails.members.map((member: any) => (
+                  <MemberCard
+                    key={member.user_id}
+                    src={member.profile_picture} 
+                    name={member.name}
+                    batch={member.batch} 
+                    role={member.role}
+                  />
+                ))}
+              </div>
             </div>
           ) : (
             <div>No members currently working on this project</div>
