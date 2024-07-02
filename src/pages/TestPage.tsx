02b39ae2a335bf3a11fa954d6be4ac9eaed5f785
Sanjay <146640686+san-jay-14@users.noreply.github.com>
2024-07-02 10:43:29 +0530

    application card and status pill

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,19 +1,11 @@
-import ApplicantCard from "@/components/ApplicantCard";
 import TopBar from "@/components/TopBar";
-import ApplicationCard from "@/components/ApplicationCard";
 
 const TestPage = () => {
   return (
     <div className="w-[100vw] h-[100vh] bg-black">
       <div className="flex"></div>
 
-      <ApplicationCard
-        date="23 March 2022"
-        name="M RAMKISHORE"
-        projectName="React Project"
-        status="Accepted"
-        role="Designer Role"
-      />
+      <TopBar />
     </div>
   );
 };
